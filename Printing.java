8ccac423480f0997a070a38cefbd5596826f41cd
Usha <81477510+usshaa@users.noreply.github.com>
2022-12-11 18:39:31 +0530

    NewCommit

--- Printing.java
+++ Printing.java
@@ -4,6 +4,7 @@ public class Printing {
  
     System.out.println("Configuring Jenkins with Github");
     System.out.println("This will build in jenkins when new commit triggers");
+    System.out.println("I am doing new commit");
     
   }
 }
