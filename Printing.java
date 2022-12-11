919e9e5e8ed9850c2f815cfddfe033200f496604
Usha <81477510+usshaa@users.noreply.github.com>
2022-12-11 18:15:50 +0530

    Create Printing.java

---
+++ Printing.java
@@ -0,0 +1,9 @@
+public class Printing {
+  
+  public static void main(String[] args){
+ 
+    System.out.println("Configuring Jenkins with Github");
+    System.out.println("This will build in jenkins when new commit triggers");
+    
+  }
+}
