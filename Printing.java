f922eacd9c4de4e84ae6d2086ededbbfa4b1ab68
Usha <81477510+usshaa@users.noreply.github.com>
2022-12-11 19:00:10 +0530

    ThirdCommit

--- Printing.java
+++ Printing.java
@@ -5,7 +5,7 @@ public class Printing {
     System.out.println("Configuring Jenkins with Github");
     System.out.println("This will build in jenkins when new commit triggers");
     System.out.println("I am doing new commit");
-    System.out.println("Successfully build in jenkins");
+    
     
   }
 }
